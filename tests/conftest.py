c6984e7f05741eb4c78d6f5ec60a1d4e4d363aed
AlexStzm <svetozargrey@ya.ru>
2023-08-05 12:17:00 +0300

    Добавлена фикстура стартовой настройки браузера

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+from selene import browser
+import pytest
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_manager():
+    browser.config.window_width = 1680
+    browser.config.window_height = 1050
+    browser.config.base_url = 'https://demoqa.com'
+    yield
+    browser.quit()
