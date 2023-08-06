23d1788e67ff27f82b75220597e5ddbb3c2804ea
AlexStzm <svetozargrey@ya.ru>
2023-08-06 21:07:32 +0300

    Улучшена читаемость кода

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
 from selene import browser
 import pytest
 
+
 @pytest.fixture(scope='function', autouse=True)
 def browser_manager():
+
     browser.config.window_width = 1680
     browser.config.window_height = 1050
     browser.config.timeout = 2
     browser.config.base_url = 'https://demoqa.com'
+
     yield
+
     browser.quit()
