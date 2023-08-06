cc8399931c050aeb1d57aedd212fa7499026b1f3
AlexStzm <svetozargrey@ya.ru>
2023-08-06 20:05:26 +0300

    Добавлен код теста формы для практики

---
+++ tests/test_automation_practice_form.py
@@ -0,0 +1,29 @@
+from selene import browser, have
+from selenium.webdriver import Keys
+
+
+def test_practice_form():
+    browser.open('/automation-practice-form')
+    browser.element('#firstName').type('FirstName')
+    browser.element('#lastName').type('LastName')
+    browser.element('#userEmail').type('test@test.com')
+    browser.element('[for=gender-radio-1]').click()
+    browser.element('#userNumber').type('9999999999')
+    browser.element('#dateOfBirthInput').type(Keys.CONTROL + 'A' + Keys.NULL + '15.nov.1991').press_enter()
+    browser.element('#subjectsInput').type('test')
+    browser.element('[for=hobbies-checkbox-1]').click()
+    browser.element('[for=hobbies-checkbox-2]').click()
+    browser.element('#uploadPicture').send_keys('C:/test.txt')
+    browser.element('#currentAddress').type('User test address')
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#react-select-4-input').type('Delhi').press_enter()
+    browser.element('#submit').press_enter()
+
+    browser.element('#example-modal-sizes-title-lg').should(have.text(
+        'Thanks for submitting the form'))
+    browser.element('.table-responsive').should(have.text(
+        'FirstName LastName' and
+        'test@test.com' and 'Male' and
+        '9999999999' and '15 November,1991' and
+        'Sports, Reading' and 'test.txt' and
+        'User test address' and 'NCR Delhi'))
