c9991159fd8bc0cbc85fedc5b8e79f235cfbc30e
AlexStzm <svetozargrey@ya.ru>
2023-08-06 19:54:49 +0300

    В фикстуру добавлен timeout

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import pytest
 def browser_manager():
     browser.config.window_width = 1680
     browser.config.window_height = 1050
+    browser.config.timeout = 2
     browser.config.base_url = 'https://demoqa.com'
     yield
     browser.quit()
